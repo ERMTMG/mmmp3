e5cb501ba8f5184dc0e39020191707c22fb33fae
Eduardo <eduardorhsiete@gmail.com>
2025-04-02 10:22:44 +0200

    added removing prefix functionality, might need to separate this into different files soon

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from functools import partial
 
 print = partial(print, sep = '', end = '')
 
+def remove_prefix_from_file(file: os.DirEntry[str], prefix: str) -> None:
+    name: str = file.name
+    if name[0:len(prefix)] == prefix:
+        newName: str = name[len(prefix):]
+        newPath: str = os.path.join(os.path.dirname(file.path), newName)
+        os.rename(file.path, newPath)
+
 def list_directory(dir: str) -> None:
     for entry in os.scandir(dir):
         if(entry.is_file()):
-            print('found file: ', entry.path, '\n')
+            print('found file: ', entry.name, '\n')
             filename, extension = os.path.splitext(entry.path)
             if(extension == '.mp3'):
                 print(f'\t{filename} is an mp3 file! Playing...\n')
@@ -19,6 +26,7 @@ def list_directory(dir: str) -> None:
                 while(stop != 'stop'):
                     stop = input()
                 player.stop()
+                remove_prefix_from_file(entry, "[SPOTDOWNLOADER.COM] ")
             else:
                 print(f'\textension: {extension}\n')
         else:
