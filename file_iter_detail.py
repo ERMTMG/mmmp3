791348fdbd45957e6353da735dc3b63ff571741f
Eduardo <eduardorhsiete@gmail.com>
2025-04-10 11:39:09 +0200

    ALPHA COMPLETE! added all the necessary functionality. still pretty barebones as a program but now it's usable

--- file_iter_detail.py
+++ file_iter_detail.py
@@ -1,7 +1,7 @@
 import os
 import inquirer
 import vlc
-from dir_iter import *
+from meta_edit import *
 from functools import partial
 
 print = partial(print, sep = '', end = '')
@@ -18,13 +18,39 @@ def remove_prefix_from_file(file: os.DirEntry[str], prefix: str) -> bool:
 
 def file_option_remove_prefix(file: os.DirEntry) -> None:
     prefix: str = inquirer.text(
-        '\t\t\tEnter the name of the prefix to remove')
+        'Enter the name of the prefix to remove')
     affected = remove_prefix_from_file(file, prefix)
     if not affected:
-        print(f'\t\t\t\tFilename wasn\'t changed - "{prefix}" is not the start of the filename!\n')
+        print(f'\tFilename wasn\'t changed - "{prefix}" is not the start of the filename!\n')
 
-        
-
+def file_option_auto_change_name(file: os.DirEntry) -> None:
+    filename, _ = os.path.splitext(file.name)
+    mp3_handle = get_mp3_file(file.path)
+    edit_mp3_name(mp3_handle, filename)
+    save_mp3_file(mp3_handle)
+    print(f'\tEdited file\'s metadata name to {filename}.\n')
+    
+def file_option_manual_edit(file: os.DirEntry) -> None:
+    fields: list[str] = inquirer.checkbox('What fields do you want to change?', choices = [
+        ('Artist name', 'artist'),
+        ('Album name', 'album'),
+        ('Song title', 'title'),
+    ], default = ['artist', 'album'])
+    function_input: dict[str, str|None] = {
+        'title' : None, 'artist' : None, 'album' : None
+    }
+    mp3_handle = get_mp3_file(file.path)
+    for field in fields:
+        field_value: str = inquirer.text(f'Enter the song\'s new {field}',
+                                         default=get_mp3_tag(mp3_handle, field))
+        function_input[field] = field_value
+    edit_mp3_tags(mp3_handle,
+                  artist = function_input['artist'],
+                  album  = function_input['album'],
+                  title  = function_input['title'])
+    save_mp3_file(mp3_handle)
+    print(f'\tFile saved successfully.\n')
+    
 
 def dir_option_list_files_detail(dir_name: str) -> None:
     file_iter: os._ScandirIterator = os.scandir(dir_name)
@@ -34,11 +60,11 @@ def dir_option_list_files_detail(dir_name: str) -> None:
         if file.is_file() and file_extension == ".mp3":
             print(f'-> \033[1;92mon MP3 file: {file.path}\n\033[0m')
             option: int = inquirer.list_input('Select an option', choices=[
-                ('\tSkip to the next file', -1),
-                ('\tListen to the file briefly', 0),
-                ('\tRemove a prefix from the file\'s filename', 1),
-                ('\tAutomatically change the song\'s metadata name to the filename', 2),
-                ('\tEdit the file\'s metadata (artist, album, name...) manually', 3),
+                ('Skip to the next file', -1),
+                ('Listen to the file briefly', 0),
+                ('Remove a prefix from the file\'s filename', 1),
+                ('Automatically change the song\'s metadata name to the filename', 2),
+                ('Edit the file\'s metadata (artist, album, name...) manually', 3),
             ])
             match option:
                 case -1:
@@ -46,9 +72,14 @@ def dir_option_list_files_detail(dir_name: str) -> None:
                 case 0:
                     audio: vlc.MediaPlayer = vlc.MediaPlayer(file.path)
                     audio.play()
-                    _: str = inquirer.text('\t\tPress enter to stop...')
+                    _: str = inquirer.text('Press enter to stop...')
                     audio.stop()
                 case 1:
                     file_option_remove_prefix(file)
+                case 2:
+                    file_option_auto_change_name(file)
+                case 3:
+                    file_option_manual_edit(file)
+                
                     
                     
--- meta_edit.py
+++ meta_edit.py
@@ -6,22 +6,35 @@ def get_mp3_file(filename: str) -> mutagen.mp3.EasyMP3:
         return fileHandle
     else:
         raise RuntimeWarning('File wasn\'t opened as MP3')
+
+def get_mp3_tag(fileHandle: mutagen.mp3.EasyMP3, tag: str) -> str:
+    output = fileHandle[tag]
+    if isinstance(output, list):
+        return output[0]
+    else:
+        return output
     
-def edit_mp3_tags(fileHandle: mutagen.easyid3.EasyID3, 
+def edit_mp3_tags(fileHandle: mutagen.mp3.EasyMP3, 
                   artist: str|None = None, 
-                  album: str|None = None) -> None:
+                  album: str|None = None,
+                  title: str|None = None) -> None:
     if artist is not None:
         fileHandle['artist'] = artist
     if album is not None:
         fileHandle['album'] = album
+    if title is not None:
+        fileHandle['title'] = title
 
-def edit_mp3_artist(fileHandle: mutagen.easyid3.EasyID3, artistName: str) -> None:
+def edit_mp3_artist(fileHandle: mutagen.mp3.EasyMP3, artistName: str) -> None:
     fileHandle['artist'] = artistName
 
-def edit_mp3_album(fileHandle: mutagen.easyid3.EasyID3, albumName: str) -> None:
+def edit_mp3_album(fileHandle: mutagen.mp3.EasyMP3, albumName: str) -> None:
     fileHandle['album'] = albumName
+
+def edit_mp3_name(fileHandle: mutagen.mp3.EasyMP3, name: str) -> None:
+    fileHandle['title'] = name
     
-def save_mp3_file(fileHandle: mutagen.easyid3.EasyID3, newFilename: str|None = None):
+def save_mp3_file(fileHandle: mutagen.mp3.EasyMP3, newFilename: str|None = None):
     if newFilename is not None:
         fileHandle.save(newFilename)
     else:
